eea5e19f7ed7db40115b0731c5702ac640b3af99
Gustavo <gustavogmfarias@gmail.com>
2022-11-12 15:27:20 -0300

    .

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+import { setCookie, parseCookies } from "nookies";
+import Router from "next/router";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredetials = {
   email: string;
@@ -7,6 +16,7 @@ type SignInCredetials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredetials): Promise<void>;
+  user?: User;
   isAuthenticated: boolean;
 };
 
@@ -17,12 +27,55 @@ type AuthProviderProps = {
 };
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      const response = api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+        setUser({ email, permissions, roles });
+      }); //rota /me porque nesse backend específico é a rota que retorna os dados do usuário logado..
+    }
+  }, []);
+
+  //a) Deixo o array de dependências vazio, porque quero que seja carregado somente uma vez.
+  //b) Depois eu pego o token, faço uma requisiçaõ a api e guardo as informações do usuário.
+  //c) se a gente for no jwt.io e jogar o token la, também temos as informações do usuário dentro do payload. Para autenticação tradicional serviria, mas com oestou lidando com permissões e cargos, coisas que pdoem mudar, eu prefiro recarregar as informações cada vez que o usuário acessa a página.. Haverá uma requisição sim a mais, mas vou garantir que vou ter as permisões e roles do usuários atualizados. SE não tivesse isso, poderia pegar direto do token.
+
   async function signIn({ email, password }: SignInCredetials) {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", { email, password }); //rota sessions depois os dados email e password
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 dias
+        path: "/",
+      });
+
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 dias
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
   }
+
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
